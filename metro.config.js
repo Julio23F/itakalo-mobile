ddf36e20e85d94eded9a40bf5544b64d9063ca94
Julio23F <juliofaralahy23@gmail.com>
2025-11-03 09:54:56 +0300

    Feat: add React viro AR

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,38 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const { withNativeWind } = require("nativewind/metro");
- 
-const config = mergeConfig(getDefaultConfig(__dirname), {
-  /* your config */
-});
- 
-module.exports = withNativeWind(config, { input: "./global.css" });
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: [
+      ...defaultConfig.resolver.assetExts,
+      "obj",
+      "mtl",
+      "JPG",
+      "vrx",
+      "hdr",
+      "gltf",
+      "glb",
+      "bin",
+      "arobject",
+      "gif",
+    ],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
